a35c80f8926eecafb1b88a8da8f0a0f04a334741
Abdallah Ahmed Sherdy <Abdullah_20220276@fci.helwan.edu.eg>
2023-11-30 11:46:29 +0200

    Create Register.java

---
+++ src/Register.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Register {
+    private String name;
+    private String password;
+    private String email;
+    private Date CheckInDate;
+    private Date CechOutDate;
+    public Register(){
+
+    }
+
+}
