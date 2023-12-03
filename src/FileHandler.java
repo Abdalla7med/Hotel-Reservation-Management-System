2e3967eb48215f415b2f6e887a4f5f15910c87b0
Abdallah Ahmed Sherdy <Abdullah_20220276@fci.helwan.edu.eg>
2023-12-03 16:35:05 +0200

    Update FileHandler.java

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -1,6 +1,6 @@
 import java.io.*;
 public class FileHandler {
-    private File file = null;
+     static File file = null;
 
     public FileHandler() { }
 
@@ -15,10 +15,17 @@ public class FileHandler {
 
     // Method to read content from a file
     public static String readFile(String filePath) {
+        StringBuilder content = new StringBuilder();
+        try(FileReader reader= new FileReader(file)){
+            String line;
+            while((line = reader.readLine())){
 
+            }
+        }catch(IOException IOE){
+            System.out.println("can't deal with this file");
+        }
         return filePath;
     }
-
     // Method to write content to a file
     public static void writeFile(String filePath, String content) {
 
