5ba84107bcf7cbfc704b1013805fb75c6a308bea
Abdallah Ahmed Sherdy <Abdullah_20220276@fci.helwan.edu.eg>
2023-12-10 21:20:31 +0200

    Update Guest.java

--- src/Guest.java
+++ src/Guest.java
@@ -1,3 +1,16 @@
 public class Guest extends Person{
-
+    public static int counter =1;
+    public Guest(){
+        counter++;
+    }
+    @Override
+    public String getContent(){
+        StringBuilder content= new StringBuilder();
+        content.append(getSSnn()).append(" , ");
+        content.append(getName()).append(" , ");
+        content.append(getEmail()).append(" , ");
+        content.append(getPhone()).append(" , ");
+        content.append(getAddress()).append(" , ");
+        return content.toString();
+    }
 }
