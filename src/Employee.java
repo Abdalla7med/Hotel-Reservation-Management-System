e3d4976c31107bfe04294dc93cb68451c53b97ba
Abdallah Ahmed Sherdy <Abdullah_20220276@fci.helwan.edu.eg>
2023-12-10 21:20:37 +0200

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,32 @@
 public class Employee extends Person {
 
+    private String position;
+    private double salary;
+    public static int counter =0;
+    Employee(){
+        counter++;
+    }
+    public void setSalary(double salary){
+        this.salary=salary;
+    }
+    public void setPosition(String position){
+
+            this.position=position;
+
+    }
+    @Override
+    public String getContent(){
+       StringBuilder content= new StringBuilder();
+       content.append(getSSnn()).append(" , ");
+       content.append(getName()).append(" , ");
+       content.append(getEmail()).append(" , ");
+       content.append(getPhone()).append(" , ");
+       content.append(getAddress()).append(" , ");
+       content.append(this.position).append(" , ");
+       content.append(this.salary).append(" , ");
+    return content.toString();
+    }
+    public void setRoomStatus(boolean status,Room target){
+        target.setStatus(status);
+    }
 }
