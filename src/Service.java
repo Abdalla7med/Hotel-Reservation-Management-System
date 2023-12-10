a28850dadafc3b5c619e52597078fa06dbdc8c10
Abdallah Ahmed Sherdy <Abdullah_20220276@fci.helwan.edu.eg>
2023-12-10 21:20:15 +0200

    Update Service.java

--- src/Service.java
+++ src/Service.java
@@ -1,14 +1,16 @@
 public class Service {
-    public Service(){}
+    public static int counter =0;
+    public Service(){
+        counter++;
+    }
     public Service(String name,String id,boolean status, double price){
         this.name=name;
         this.id=id;
         this.status=status;
         this.price=price;
+        counter++;
     }
     static int use=1; // will be incremented when new object is instantiated
-
-
     private String name;
     private boolean status;
     private double price;
@@ -28,5 +30,13 @@ public class Service {
     public void getUsage(){
         System.out.println("this service is used for : "+ use);
         System.out.println("and total benefits of this service is :"+use*price);
+    }
+    public String getContent(){
+        StringBuilder content= new StringBuilder();
+        content.append(id).append(" , ");
+        content.append(name).append(" , ");
+        content.append(price).append(" , ");
+        content.append("\n");
+        return content.toString();
     }
 }
