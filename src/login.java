76f476ce79f6795929d21ffda9e112a2e4979eff
Abdallah Ahmed Sherdy <Abdullah_20220276@fci.helwan.edu.eg>
2023-12-11 17:59:47 +0200

    Update login.java

--- src/login.java
+++ src/login.java
@@ -1,5 +1,19 @@
-public class login {
-    public login(){
+import java.util.Scanner;
 
-    }
-}
+public class login {
+    Scanner in=new Scanner(System.in);
+    FileHandler handler=new FileHandler();
+    public login(){}
+    public login(String file){
+
+         System.out.println("enter Id: "); String id=in.nextLine();
+         if(handler.Exist(file,id)){
+             String line=handler.CertainRecord(MainPage.empFile,id);
+             String []data=line.split(",");
+             System.out.println("Welcome "+data[1].trim());
+         }else{
+             System.out.println("Incorrect Id");
+         }
+      }
+
+} // end of class
