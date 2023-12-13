c9ad02ec054f73b21d2e1432cc0a8c50ea78bbaf
Abdallah Ahmed Sherdy <Abdullah_20220276@fci.helwan.edu.eg>
2023-12-13 10:02:32 +0200

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -2,9 +2,15 @@ public class Employee extends Person {
 
     private String position;
     private double salary;
+    private static  int SSnn=FileHandler.nOofLines(MainPage.empFile);
     public static int counter =0;
+
     Employee(){
+        SSnn++;
         counter++;
+    }
+    public int getSSn(){
+        return this.SSnn;
     }
     public void setSalary(double salary){
         this.salary=salary;
@@ -17,13 +23,17 @@ public class Employee extends Person {
     @Override
     public String getContent(){
        StringBuilder content= new StringBuilder();
-       content.append(getSSnn()).append(" , ");
+       content.append(getSSn()).append(" , ");
        content.append(getName()).append(" , ");
        content.append(getEmail()).append(" , ");
        content.append(getAddress()).append(" , ");
        content.append(this.position).append(" , ");
        content.append(this.salary);
     return content.toString();
+    }
+    @Override
+    public String toString(){
+        return this.getName();
     }
     public void setRoomStatus(boolean status,Room target){
         target.setAvailable(status);
