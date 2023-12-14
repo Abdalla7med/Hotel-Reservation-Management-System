ba724f5fcda08541c653bbd5d6c75362217a4bd8
Abdallah Ahmed Sherdy <Abdullah_20220276@fci.helwan.edu.eg>
2023-12-14 11:57:16 +0200

    Update RegularRoom.java

--- src/RegularRoom.java
+++ src/RegularRoom.java
@@ -1,12 +1,16 @@
 public class RegularRoom extends Room{
+
     public RegularRoom(){
-        super();
+
+        super.setRoomNum(IdGenerator.getNextId(MainPage.RoomFile));
     }
-    public RegularRoom(String roomNum, double price, boolean wifi, boolean breakfast) {
-        super(roomNum, price, wifi, breakfast); // call the superclass constructor
+    public RegularRoom( double price, boolean wifi, boolean breakfast) {
+        super(String.valueOf(IdGenerator.getNextId(MainPage.RoomFile)), price, wifi, breakfast); // call the superclass constructor
     }
 
     // The override method for getting the specifications of the regular room
+    // getSpecification differ from getContent because getSpecification used to filter room only we can use getSpecification in getContent method
+    // but just to keep it simple and separate each task
     @Override
     public String getSpecifications() {
         return "Regular room with " + (hasWifi() ? "wifi" : "no wifi") + " and " + (hasBreakfast() ? "breakfast" : "no breakfast");
